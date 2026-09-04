2cda146673ae34b2c3f145f7300d18816ef4e8f0
Andy Din <andy.din@premierdata.com.au>
2026-09-04 17:27:16 +1000

    Topbar: remove hardcoded Andy/Administrator user block (redundant with sidebar footer, and it was static)

--- reporting-ui/components/layout/Topbar.tsx
+++ reporting-ui/components/layout/Topbar.tsx
@@ -10,26 +10,6 @@ export default function TopBar() {
 
       </div>
 
-      <div className="flex items-center gap-3">
-
-        <div className="text-right">
-
-          <div className="text-sm font-medium text-white">
-            Andy
-          </div>
-
-          <div className="text-xs text-slate-400">
-            Administrator
-          </div>
-
-        </div>
-
-        <div className="flex h-10 w-10 items-center justify-center rounded-full bg-sky-600 font-semibold text-white">
-          A
-        </div>
-
-      </div>
-
     </header>
   );
 }
